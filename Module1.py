1af833b63afe5998617ab5882ca09f5aa97866b5
Mrinmoy Samanta <mrinmoysamantamrinmoysamanta@gmail.com>
2024-11-30 13:55:45 +0530

    Add files via upload
    Python Difference and Mathematical Modules

---
+++ Module1.py
@@ -0,0 +1,14 @@
+# Importing module as well as renaming it
+import math as m
+
+print ("The value of pi is: ", m.pi)
+
+# usage of from 
+from math import pi
+
+print("The value of pi is: ", pi)
+
+# import all names from the standard module name
+from math import *
+
+print("The value of pi is: ", pi)
